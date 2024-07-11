6c66203cb7c667a3ca5e8b637c6f9218837b4a8f
chaman1998 <chamansarkar11@gmail.com>
2024-07-11 17:59:52 +0530

    Header structure ready

---
+++ Pixstock/js/theme.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const $HTML = document.documentElement;
+
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if(sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else{
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
