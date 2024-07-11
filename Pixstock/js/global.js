0f90ac5d42ce6b684db6f406775950d3faa033b2
chaman1998 <chamansarkar11@gmail.com>
2024-07-11 19:19:40 +0530

    add scroll effect js

--- Pixstock/js/global.js
+++ Pixstock/js/global.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// Header on-Scroll state
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
