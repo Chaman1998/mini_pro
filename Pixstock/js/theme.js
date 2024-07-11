0fdfe4ccb2ff30b2db20e11c8e2bdec97b2b161e
chaman1998 <chamansarkar11@gmail.com>
2024-07-11 19:06:32 +0530

    Add the theme code for dark/light mode

--- Pixstock/js/theme.js
+++ Pixstock/js/theme.js
@@ -4,8 +4,20 @@ const $HTML = document.documentElement;
 
 let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-if(sessionStorage.getItem("theme")){
+if (sessionStorage.getItem("theme")) {
     $HTML.dataset.theme = sessionStorage.getItem("theme");
-}else{
+} else {
     $HTML.dataset.theme = isDark ? "dark" : "light";
 }
+
+const changeTheme = function() {
+    // console.log("click");
+    isDark = sessionStorage.getItem("theme");
+    sessionStorage.setItem("theme", isDark === "light" ? "dark" : "light");
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+
+window.addEventListener("load", ()=> {
+    const $themeBtn = document.querySelector("[data-theme-toggler]");
+    $themeBtn.addEventListener("click",changeTheme);
+})
